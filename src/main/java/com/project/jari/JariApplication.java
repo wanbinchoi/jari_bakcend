b0c9f461a877cc95486c247ef9b7087218455fb8
wanbinchoi <cwb6278@gmail.com>
2025-09-23 16:02:45 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/jari/JariApplication.java
@@ -0,0 +1,13 @@
+package com.project.jari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jari/JariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
