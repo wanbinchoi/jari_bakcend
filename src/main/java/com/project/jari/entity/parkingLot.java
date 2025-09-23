7d50cb31b6b0c5df221d3b0afa315f18c5f20a51
wanbinchoi <cwb6278@gmail.com>
2025-09-23 21:38:07 +0900

    프로젝트 환경 구성

--- src/main/java/com/project/jari/entity/parkingLot.java
+++ src/main/java/com/project/jari/entity/parkingLot.java
@@ -1,5 +1,13 @@
 package com.project.jari.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class parkingLot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pkltCode;
 }
